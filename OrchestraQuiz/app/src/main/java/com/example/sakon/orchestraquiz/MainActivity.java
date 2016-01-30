602e31be42ba8b2e7e6ab75e88ff521748150da1
gunki43 <k204400@gmail.com>
2016-01-31 00:04:26 +0900

    クリックイベントを作成

--- OrchestraQuiz/app/src/main/java/com/example/sakon/orchestraquiz/MainActivity.java
+++ OrchestraQuiz/app/src/main/java/com/example/sakon/orchestraquiz/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.sakon.orchestraquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.button1);
+        Button button2 = (Button)findViewById(R.id.button2);
+        Button button3 = (Button)findViewById(R.id.button3);
+        Button button4 = (Button)findViewById(R.id.button4);
+
+        textView = (TextView)findViewById(R.id.textView1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("ア");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("イ");
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("ウ");
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("エ");
+            }
+        });
     }
 
 }
