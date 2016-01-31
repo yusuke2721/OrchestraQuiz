90bc65c82338fa8e77cb57f4467836a10558e2d2
yusuke2721 <yusukes@hotmail.co.jp>
2016-02-01 00:11:46 +0900

    JavaDoc、コメントを書きました

--- OrchestraQuiz/app/src/main/java/com/example/sakon/orchestraquiz/CSVParser.java
+++ OrchestraQuiz/app/src/main/java/com/example/sakon/orchestraquiz/CSVParser.java
@@ -2,20 +2,22 @@ package com.example.sakon.orchestraquiz;
 
 import android.content.Context;
 import android.content.res.AssetManager;
-import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class CSVParser {
 
+    /**
+     * CSVファイルを読み込み、Questionクラスのリストを作成する
+     * @param context
+     * @param questionList
+     */
     public static void parse(Context context, List<Question> questionList) {
-        System.out.println("CSV読込メソッド開始");
 
         // AssetManagerの呼び出し
         AssetManager assetManager = context.getResources().getAssets();
@@ -25,10 +27,11 @@ public class CSVParser {
             InputStreamReader inputStreamReader = new InputStreamReader(is);
             BufferedReader bufferReader = new BufferedReader(inputStreamReader);
             String line = "";
+
             while ((line = bufferReader.readLine()) != null) {
-                // 各行が","で区切られていて4つの項目があるとする
-                StringTokenizer st = new StringTokenizer(line, ",");
                 Question question = new Question();
+                StringTokenizer st = new StringTokenizer(line, ",");
+                // MusicalSymbolクラスを作成し、4つ1組でQuestionクラスのリストを作成
                 for (int i = 0; i < 4; i++) {
                     MusicalSymbol musicalSymbol = new MusicalSymbol(st.nextToken(), st.nextToken());
                     question.addChoices(musicalSymbol);
@@ -36,10 +39,9 @@ public class CSVParser {
                 questionList.add(question);
             }
             bufferReader.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println("CSV読込メソッド終了");
     }
 }
