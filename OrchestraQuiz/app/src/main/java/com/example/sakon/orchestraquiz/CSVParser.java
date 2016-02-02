d30f9ad71fc6db33a4d4353b75738fafd14210ce
yusuke2721 <yusukes@hotmail.co.jp>
2016-02-03 00:09:01 +0900

    出題順をランダム化 CSV読込後、Listをシャッフル

--- OrchestraQuiz/app/src/main/java/com/example/sakon/orchestraquiz/CSVParser.java
+++ OrchestraQuiz/app/src/main/java/com/example/sakon/orchestraquiz/CSVParser.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -38,6 +39,7 @@ public class CSVParser {
                 }
                 questionList.add(question);
             }
+            Collections.shuffle(questionList);
             bufferReader.close();
 
         } catch (IOException e) {
